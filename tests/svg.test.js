fcf1e16fb6a39e1b1b78211cdf93443fe8cf2d03
neyneyalldayday <drfrankenstein123@gmail.com>
2023-10-26 11:31:20 -0500

    test are working

--- tests/svg.test.js
+++ tests/svg.test.js
@@ -1,27 +1,52 @@
 const SVG = require("../utils/svg");
-const { Circle } = require("../utils/shapes");
+const { Circle, Square, Triangle } = require("../utils/shapes");
 
-describe("Whole SVG", () => {
-  let svg;
-
-  beforeEach(() => {
-    svg = new SVG();
+describe('SVG Class', () => {
+  it('should render an empty SVG', () => {
+    const svg = new SVG();
+    const result = svg.renderSvg();
+    expect(result).toEqual('<svg width="100" height="100" xmlns="http://www.w3.org/2000/svg"></svg>');
   });
 
-  it("should render an SVG with the set text and shape", () => {
-    svg.setText("blue", "Sup");
-    svg.setShape(new Circle()); // Use Circle class instance here
-    const renderedSvg = svg.renderSvg();
-    expect(renderedSvg).toEqual(
-      '<svg width="100" height="100" xmlns="http://www.w3.org/2000/svg"><circle cx="50" cy="50" r="40" fill="blue"/><text x="50" y="50" font-family="Arial" text-anchor="middle" alignment-baseline="middle" font-size="24" fill="blue">Sup</text></svg>'
-    );
+  it('should set text in the SVG', () => {
+    const svg = new SVG();
+    svg.setText('red', 'Hello');
+    const result = svg.renderSvg();
+    expect(result).toContain('<text x="50" y="50" font-family="Arial" text-anchor="middle" alignment-baseline="middle" font-size="24" fill="red">Hello</text>');
   });
 
-  it("should render an empty SVG without text and shape method", () => {
-    const renderedSvg = svg.renderSvg();
-    expect(renderedSvg).toEqual(
-      '<svg width="100" height="100" xmlns="http://www.w3.org/2000/svg"></svg>'
-    );
+  it('should set a shape in the SVG', () => {
+    const svg = new SVG();
+    const circle = new Circle('blue');
+    svg.setShape(circle);
+    const result = svg.renderSvg();
+    expect(result).toContain('<circle cx="50" cy="50" r="40" fill="blue" />');
+  });
+});
+
+describe('Shape Classes', () => {
+  it('should render a Circle shape', () => {
+    const circle = new Circle('green');
+    const result = circle.render();
+    expect(result).toContain('<circle cx="50" cy="50" r="40" fill="green" />');
+  });
+
+  it('should render a Square shape', () => {
+    const square = new Square('purple');
+    const result = square.render();
+    expect(result).toContain('<rect x="10" y="10" width="80" height="80" fill="purple" />');
+  });
+
+  it('should render a Triangle shape', () => {
+    const triangle = new Triangle('orange');
+    const result = triangle.render();
+    expect(result).toContain('<polygon points="50,10 10,90 90,90" fill="orange" />');
+  });
+
+  it('should set the shape color', () => {
+    const circle = new Circle('blue');
+    circle.setShapeColor('red');
+    expect(circle.color).toEqual('red');
   });
 });
 
